f0fc0e82feba1b511376a38ffe0343d07c36ad90
piquark6046 <j.w.so@adguard.com>
2024-08-10 01:28:50 +0000

    feat: Add ts-morph and eslint types to package.json

---
+++ sources/index.ts
@@ -0,0 +1,54 @@
+import * as TsMorph from 'ts-morph'
+
+export type IExtractorOptions = {
+  CheckRegExToUseReverse: boolean
+  UseBuiltInDeobfuscator: boolean
+}
+export class Extractor {
+  protected Code: string = ''
+  
+  constructor(Code: string) {
+    this.Code = Code
+  }
+
+  GetToken(): string {
+    const ProjectInstance = new TsMorph.Project({
+      compilerOptions: {
+        allowJs: true,
+        skipLibCheck: true,
+        target: TsMorph.ScriptTarget.ES2020
+      },
+      skipFileDependencyResolution: true,
+      useInMemoryFileSystem: true
+    })
+    const FileInstance = ProjectInstance.createSourceFile('code.js', this.Code, { overwrite: true })
+    const Tokens: string[] = []
+  
+    FileInstance.forEachDescendant(CbNode => {
+      if (CbNode.getKind() === TsMorph.SyntaxKind.VariableDeclaration) {
+        const VariableDeclaration = CbNode.asKind(TsMorph.SyntaxKind.VariableDeclaration)
+        const Initalizer = VariableDeclaration.getInitializer()
+  
+        if (Initalizer && Initalizer.getKind() === TsMorph.SyntaxKind.StringLiteral) {
+          const Value = Initalizer.getText().slice(1, -1)
+          if (/^eyJ[A-Za-z0-9._-]+$/.test(Value)) {
+            VariableDeclaration.findReferencesAsNodes().forEach(Reference => {
+              const TokenVars: TsMorph.KindToNodeMappings[TsMorph.ts.SyntaxKind.Identifier][] = []
+              Reference.getParent().getChildren().forEach(Child => {
+                TokenVars.push(...Child.getChildrenOfKind(TsMorph.SyntaxKind.Identifier))
+              })
+              TokenVars.forEach(TokenVar => {
+                TokenVar.getDefinitions()[0].getDeclarationNode().forEachChild(Child => {
+                  if (Child.getKind() === TsMorph.SyntaxKind.StringLiteral) {
+                    Tokens.push(Child.getText().slice(1, -1))
+                  }
+                })
+              })
+            })
+          }
+        }
+      }
+    })
+    return Tokens.join('')
+  }
+}
